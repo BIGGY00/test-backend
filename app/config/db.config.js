ad1ae3cebe5c44b6ccb7c76eb779247742358348
BIGGY00 <bowbigzaza2014@gmail.com>
2024-09-15 17:49:46 +0700

    config DB

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports ={
-    HOST: "b8yk85cjetmrjczebhow-mysql.services.clever-cloud.com",
-    USER: "uczirkppf2pelurm",
-    PASSWORD: "4QoIQ0eR1gOx3GzjHi4g",
-    DB: "b8yk85cjetmrjczebhow",
+    HOST: "blsamevdb2acvm0u9jhc-mysql.services.clever-cloud.com",
+    USER: "ua1otwfshxjmlwff",
+    PASSWORD: "UTb4RlMQQkzrWljZJz36",
+    DB: "blsamevdb2acvm0u9jhc",
     PORT: "3306",
     // HOST: "sql.freedb.tech",
     // USER: "freedb_user_api",
